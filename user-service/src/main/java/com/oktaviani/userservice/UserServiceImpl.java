676cf10f82cbf585e1aaece84bbc7be1fdf4d3b8
Oktavianipratiwi <113502499+Oktavianipratiwi@users.noreply.github.com>
2022-10-11 10:46:32 +0700

    Add files via upload

---
+++ user-service/src/main/java/com/oktaviani/userservice/UserServiceImpl.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.oktaviani.userservice;
+
+
+import com.oktaviani.userservice.UserService.UserService;
+import com.oktaviani.userservice.dto.User;
+import com.oktaviani.userservice.repository.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author user
+ */
+@Service
+public class UserServiceImpl implements UserService {
+  
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.getAllUsers();
+    }
+
+    @Override
+    public User getUser(String id) {
+        return userRepository.getUser(id);
+    }
+
+    @Override
+    public void addUser(User user) {
+        userRepository.addUser(user);
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return userRepository.updateUser(user);
+    }
+
+    @Override
+    public User deleteUser(String id) {
+        return userRepository.deleteUser(id);
+    }
+    
+}
